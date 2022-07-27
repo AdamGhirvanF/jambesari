d3c085552a328dbf21088bf60ceaef56f7b93f09
Farih Akmal Haqiqi <farih.282828@gmail.com>
2022-07-27 21:50:18 +0700

    adjusting some elements

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -16,37 +16,37 @@
                 <ul class="nav">
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Profil.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/profil">Profil Desa</a>
                     </div>
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Struktur.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/struktur">Struktur Desa</a>
                     </div>
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Program.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/program">Program</a>
                     </div>
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Berita.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/berita">Berita</a>
                     </div>
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Galeri.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/galeri">Galeri</a>
                     </div>
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Layanan.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/layanan">Layanan</a>
                     </div>
                     <div class="nav-link pb-2">
                         <img src="{{ URL::asset('icon/navbar/Kontak.svg'); }}"
-                            width="18px" class="me-2">
+                            width="25px" class="me-2">
                         <a href="/kontak">Kontak</a>
                     </div>
                 </ul>
