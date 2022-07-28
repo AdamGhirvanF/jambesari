58896adecaa54c0beb65f59347f882ad6ea31148
Farih Akmal Haqiqi <farih.282828@gmail.com>
2022-07-28 09:50:59 +0700

    update size image berita

--- resources/views/frontend/berita.blade.php
+++ resources/views/frontend/berita.blade.php
@@ -10,32 +10,7 @@
                     <div class="col-md-4">
                         <div class="card d-flex mb-3">
                             <div class="thumbnail">
-                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" alt="Berita 1">
-                            </div>
-                            <div class="card-body">
-                                <div class="date mb-3">Senin, 22 Juni 2022</div>
-                                <a href="/berita/one" class="stretched-link title">Ada Apa
-                                    Dengan Cinta?</a>
-                                <div class="content mb-3">Lorem ipsum dolor sit,
-                                    amet
-                                    consectetur adipisicing elit. Omnis eius
-                                    voluptatibus deserunt iusto praesentium
-                                    neque perferendis blanditiis vero maxime
-                                    minima nihil tempore, a excepturi, culpa
-                                    repudiandae dolores assumenda suscipit
-                                    officiis?</div>
-                                <div class="next">
-                                    <a href="/berita/one" class="stretched-link">Baca
-                                        Selengkapnya</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-
-                        <div class="card d-flex mb-3">
-                            <div class="thumbnail">
-                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" alt="Berita 1">
+                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" class="card-img-top" alt="Berita 1">
                             </div>
                             <div class="card-body">
                                 <div class="date mb-3">Senin, 22 Juni 2022</div>
@@ -59,7 +34,31 @@
                     <div class="col-md-4">
                         <div class="card d-flex mb-3">
                             <div class="thumbnail">
-                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" alt="Berita 1">
+                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" class="card-img-top" alt="Berita 1">
+                            </div>
+                            <div class="card-body">
+                                <div class="date mb-3">Senin, 22 Juni 2022</div>
+                                <a href="/berita/one" class="stretched-link title">Ada Apa
+                                    Dengan Cinta?</a>
+                                <div class="content mb-3">Lorem ipsum dolor sit,
+                                    amet
+                                    consectetur adipisicing elit. Omnis eius
+                                    voluptatibus deserunt iusto praesentium
+                                    neque perferendis blanditiis vero maxime
+                                    minima nihil tempore, a excepturi, culpa
+                                    repudiandae dolores assumenda suscipit
+                                    officiis?</div>
+                                <div class="next">
+                                    <a href="/berita/one" class="stretched-link">Baca
+                                        Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card d-flex mb-3">
+                            <div class="thumbnail">
+                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" class="card-img-top" alt="Berita 1">
                             </div>
                             <div class="card-body">
                                 <div class="date mb-3">Senin, 22 Juni 2022</div>
@@ -82,7 +81,7 @@
                     <div class="col-md-4">
                         <div class="card d-flex mb-3">
                             <div class="thumbnail">
-                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" alt="Berita 1">
+                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" class="card-img-top" alt="Berita 1">
                             </div>
                             <div class="card-body">
                                 <div class="date mb-3">Senin, 22 Juni 2022</div>
@@ -105,7 +104,7 @@
                     <div class="col-md-4">
                         <div class="card d-flex mb-3">
                             <div class="thumbnail">
-                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" alt="Berita 1">
+                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" class="card-img-top" alt="Berita 1">
                             </div>
                             <div class="card-body">
                                 <div class="date mb-3">Senin, 22 Juni 2022</div>
@@ -128,7 +127,7 @@
                     <div class="col-md-4">
                         <div class="card d-flex mb-3">
                             <div class="thumbnail">
-                                <img src="{{ URL::asset('img/berita/thumbnail/1.png'); }}" alt="Berita 1">
+                                <img src="{{ URL::asset('img/gallery/1.png'); }}" class="card-img-top" alt="Berita 1">
                             </div>
                             <div class="card-body">
                                 <div class="date mb-3">Senin, 22 Juni 2022</div>
